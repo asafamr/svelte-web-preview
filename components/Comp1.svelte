dad1aedecbb2076322244ebb65ab57b61756409f
Asaf Amrami <asaf.amrami@gmail.com>
2021-11-04 14:34:58 +0200

    init

---
+++ components/Comp1.svelte
@@ -0,0 +1,23 @@
+<script context="module">
+    export const EXPORTED_STUFF = "LORIS OPOSUM";
+</script>
+
+<script lang="ts">
+    export let somearg = "strarg";
+</script>
+
+{#each [1, 2, 3] as num}
+    <div class="someclass">
+        {somearg}
+        <div>===</div>
+    </div>
+{/each}
+
+<style>
+    .someclass {
+        color: red;
+    }
+    div > div {
+        color: sienna;
+    }
+</style>
