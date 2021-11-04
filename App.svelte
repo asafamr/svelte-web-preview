d8a434f6822ca0d318efad9060aa777101eb0b7d
Asaf Amrami <asaf.amrami@gmail.com>
2021-11-04 14:38:34 +0200

    init

--- App.svelte
+++ App.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
     import Comp1, { EXPORTED_STUFF } from "./components/Comp1.svelte";
-    async function afunc(inp:string): Promise<number>{
+    async function afunc(inp: string): Promise<number> {
         return 5;
     }
 </script>
 
 {EXPORTED_STUFF}
 <Comp1 />
-
+{afunc("a")}
 <style>
     .unused {
         color: red;
-   
+
 </style>
